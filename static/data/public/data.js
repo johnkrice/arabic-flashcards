eadfc6ba6bca757fff3a38c25ee51aa02152ac3b
John Rice <70546018+johnkrice@users.noreply.github.com>
2020-09-24 17:26:51 -0500

    Update data.js

--- static/data/public/data.js
+++ static/data/public/data.js
@@ -1,1 +1,527 @@
-var data = [{"id":1,"arabic":"当然","english":"of course","transcribed":"dāngrán","category":"L18D1-Sports"},{"id":2,"arabic":"胖","english":"fat","transcribed":"pàng","category":"L18D1-Sports"},{"id":3,"arabic":"怕","english":"to fear, to be afraid of","transcribed":"pà","category":"L18D1-Sports"},{"id":4,"arabic":"简单","english":"simple","transcribed":"jiǎndān","category":"L18D1-Sports"},{"id":5,"arabic":"跑步","english":"to jog","transcribed":"pǎo bù","category":"L18D1-Sports"},{"id":6,"arabic":"跑","english":"to run","transcribed":"pǎo","category":"L18D1-Sports"},{"id":7,"arabic":"难受","english":"hard to bear, uncomfortable","transcribed":"nánshòu","category":"L18D1-Sports"},{"id":8,"arabic":"网球","english":"tennis","transcribed":"wǎngqiú","category":"L18D1-Sports"},{"id":9,"arabic":"拍","english":"racket","transcribed":"pāi","category":"L18D1-Sports"},{"id":10,"arabic":"篮球","english":"basketball","transcribed":"lánqiú","category":"L18D1-Sports"},{"id":11,"arabic":"游泳","english":"to swim","transcribed":"yóu yǒng","category":"L18D1-Sports"},{"id":12,"arabic":"危险","english":"dangerous","transcribed":"wēixiǎn","category":"L18D1-Sports"},{"id":13,"arabic":"淹死","english":"to drown","transcribed":"yān sǐ","category":"L18D1-Sports"},{"id":14,"arabic":"愿意","english":"to be willing","transcribed":"yuànyì","category":"L18D1-Sports"}];
+var data = [
+  {
+    "id": 1,
+    "chinese": "当然",
+    "english": "of course",
+    "pinyin": "dāngrán",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 2,
+    "chinese": "胖",
+    "english": "fat",
+    "pinyin": "pàng",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 3,
+    "chinese": "怕",
+    "english": "to fear, to be afraid of",
+    "pinyin": "pà",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 4,
+    "chinese": "简单",
+    "english": "simple",
+    "pinyin": "jiǎndān",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 5,
+    "chinese": "跑步",
+    "english": "to jog",
+    "pinyin": "pǎo bù",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 6,
+    "chinese": "跑",
+    "english": "to run",
+    "pinyin": "pǎo",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 7,
+    "chinese": "难受",
+    "english": "hard to bear, uncomfortable",
+    "pinyin": "nánshòu",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 8,
+    "chinese": "网球",
+    "english": "tennis",
+    "pinyin": "wǎngqiú",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 9,
+    "chinese": "拍",
+    "english": "racket",
+    "pinyin": "pāi",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 10,
+    "chinese": "篮球",
+    "english": "basketball",
+    "pinyin": "lánqiú",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 11,
+    "chinese": "游泳",
+    "english": "to swim",
+    "pinyin": "yóu yǒng",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 12,
+    "chinese": "危险",
+    "english": "dangerous",
+    "pinyin": "wēixiǎn",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 13,
+    "chinese": "淹死",
+    "english": "to drown",
+    "pinyin": "yān sǐ",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 14,
+    "chinese": "愿意",
+    "english": "willing",
+    "pinyin": "yuànyì",
+    "category": "L18D1-Sports"
+  },
+  {
+    "id": 15,
+    "chinese": "上大学",
+    "english": "to attend college/university",
+    "pinyin": "shàng dàxué",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 16,
+    "chinese": "为了",
+    "english": "for the sake of",
+    "pinyin": "wèile",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 17,
+    "chinese": "提高",
+    "english": "to improve, to raise, to heighten",
+    "pinyin": "tígāo",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 18,
+    "chinese": "水平",
+    "english": "level standard",
+    "pinyin": "shuǐpíng",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 19,
+    "chinese": "足球",
+    "english": "soccer, football",
+    "pinyin": "zúqiú",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 20,
+    "chinese": "比赛",
+    "english": "game, match, competition, to compete",
+    "pinyin": "bǐsài",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 21,
+    "chinese": "国际",
+    "english": "international",
+    "pinyin": "guójì",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 22,
+    "chinese": "美式",
+    "english": "American-style",
+    "pinyin": "Měishì",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 23,
+    "chinese": "应该",
+    "english": "should, ought to",
+    "pinyin": "yīnggāi",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 24,
+    "chinese": "脚",
+    "english": "foot",
+    "pinyin": "jiǎo",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 25,
+    "chinese": "踢",
+    "english": "to kick",
+    "pinyin": "tī",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 26,
+    "chinese": "手",
+    "english": "hand",
+    "pinyin": "shǒu",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 27,
+    "chinese": "抱",
+    "english": "to hold or carry in the arms",
+    "pinyin": "bào",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 28,
+    "chinese": "压",
+    "english": "to press, to hold down, to weigh down",
+    "pinyin": "yā",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 29,
+    "chinese": "被",
+    "english": "by",
+    "pinyin": "bèi",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 30,
+    "chinese": "担心",
+    "english": "to worry",
+    "pinyin": "dān xīn",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 31,
+    "chinese": "棒",
+    "english": "fantastic, super",
+    "pinyin": "bàng",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 32,
+    "chinese": "运动服",
+    "english": "sportswear, athletic clothing",
+    "pinyin": "yùndòngfú",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 33,
+    "chinese": "半天",
+    "english": "half a day, long time",
+    "pinyin": "bàntiān",
+    "category": "L18D2-Sports"
+  },
+  {
+    "id": 34,
+    "chinese": "一房一厅",
+    "english": "one bedroom and one living room",
+    "pinyin": "yì fáng yì tīng",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 35,
+    "chinese": "干净",
+    "english": "clean",
+    "pinyin": "gānjìng",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 36,
+    "chinese": "沙发",
+    "english": "sofa",
+    "pinyin": "shāfā",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 37,
+    "chinese": "饭桌",
+    "english": "dining table",
+    "pinyin": "fànzhuō",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 38,
+    "chinese": "椅子",
+    "english": "chair",
+    "pinyin": "yǐzi",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 39,
+    "chinese": "书桌",
+    "english": "desk",
+    "pinyin": "shūzhuō",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 40,
+    "chinese": "书架",
+    "english": "bookshelf, bookcase",
+    "pinyin": "shūjià",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 41,
+    "chinese": "哪里",
+    "english": "there",
+    "pinyin": "nàli",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 42,
+    "chinese": "安静",
+    "english": "quiet",
+    "pinyin": "ānjìng",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 43,
+    "chinese": "房租",
+    "english": "rent",
+    "pinyin": "fángzū",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 44,
+    "chinese": "元",
+    "english": "yuan",
+    "pinyin": "yuán",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 45,
+    "chinese": "美元",
+    "english": "US dollar",
+    "pinyin": "měiyuán",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 46,
+    "chinese": "人民币",
+    "english": "Renminbi",
+    "pinyin": "rénmínbì",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 47,
+    "chinese": "人民",
+    "english": "people",
+    "pinyin": "rénmín",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 48,
+    "chinese": "币",
+    "english": "currency",
+    "pinyin": "bì",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 49,
+    "chinese": "差不多",
+    "english": "almost",
+    "pinyin": "chàbuduō",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 50,
+    "chinese": "费",
+    "english": "fee",
+    "pinyin": "fèi",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 51,
+    "chinese": "押金",
+    "english": "security deposit",
+    "pinyin": "yājīn",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 52,
+    "chinese": "当",
+    "english": "to serve as, to be",
+    "pinyin": "dāng",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 53,
+    "chinese": "还",
+    "english": "to return (something)",
+    "pinyin": "huán",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 54,
+    "chinese": "另外",
+    "english": "furthermore, in addition",
+    "pinyin": "lìngwài",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 55,
+    "chinese": "准",
+    "english": "to allow, to be allowed",
+    "pinyin": "zhǔn",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 56,
+    "chinese": "养",
+    "english": "to raise",
+    "pinyin": "yǎng",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 57,
+    "chinese": "宠物",
+    "english": "pet",
+    "pinyin": "chǒngwù",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 58,
+    "chinese": "兴趣",
+    "english": "interest",
+    "pinyin": "xìngqù",
+    "category": "L17D2-RentingApartment"
+  },
+  {
+    "id": 59,
+    "chinese": "吵",
+    "english": "to quarrel, noisy",
+    "pinyin": "chǎo",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 60,
+    "chinese": "连",
+    "english": "even",
+    "pinyin": "lián",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 61,
+    "chinese": "做饭",
+    "english": "to cook, to prepare a meal",
+    "pinyin": "zuò fàn",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 62,
+    "chinese": "报纸",
+    "english": "newspaper",
+    "pinyin": "bàozhǐ",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 63,
+    "chinese": "广告",
+    "english": "advertisement",
+    "pinyin": "guǎnggào",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 64,
+    "chinese": "附近",
+    "english": "vicinity, nearghborhood, nearby area",
+    "pinyin": "fùjìn",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 65,
+    "chinese": "套",
+    "english": "mw for sets",
+    "pinyin": "tào",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 66,
+    "chinese": "公寓",
+    "english": "Apartment",
+    "pinyin": "gōngyù",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 67,
+    "chinese": "出租",
+    "english": "to rent out",
+    "pinyin": "chūzū",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 68,
+    "chinese": "走路",
+    "english": "to walk",
+    "pinyin": "zǒu lù",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 69,
+    "chinese": "分钟",
+    "english": "minute",
+    "pinyin": "fēnzhōng",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 70,
+    "chinese": "卧室",
+    "english": "bedroom",
+    "pinyin": "wòshì",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 71,
+    "chinese": "厨房",
+    "english": "kitchen",
+    "pinyin": "chúfáng",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 72,
+    "chinese": "卫生间",
+    "english": "bathroom",
+    "pinyin": "wèishēngjiān",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 73,
+    "chinese": "客厅",
+    "english": "living room",
+    "pinyin": "kètīng",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 74,
+    "chinese": "家具",
+    "english": "Furniture",
+    "pinyin": "jiājù",
+    "category": "L17D1-RentingApartment"
+  },
+  {
+    "id": 75,
+    "chinese": "可能",
+    "english": "may, possible",
+    "pinyin": "kěnéng",
+    "category": "L17D1-RentingApartment"
+  }
+];
